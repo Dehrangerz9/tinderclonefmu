c61a0bc545c4e0180b44e49b2fa44aa906548d90
Yume Fernandes <yumefernandes.dev@gmail.com>
2025-04-28 02:48:55 -0300

    compatibilidade-vercel

--- src/Views/matches.php
+++ src/Views/matches.php
@@ -2,7 +2,7 @@
 <ul>
   <?php foreach ($matches as $match): ?>
     <li>
-      <img src="/public/assets/images/<?= $match['foto'] ?>" width="50" />
+      <img src="/api/assets/images/<?= $match['foto'] ?>" width="50" />
       <?= $match['nome'] ?> - <a href="/mensagens?user=<?= $match['id'] ?>">Conversar</a>
     </li>
   <?php endforeach; ?>
